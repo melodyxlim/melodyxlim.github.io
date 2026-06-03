b09fb45a8eabbdad68fb5d278cdeeb195f81118b
melodyxlim <148588475+melodyxlim@users.noreply.github.com>
2026-06-03 20:55:33 +0000

    Deploying to gh-pages from @ melodyxlim/melodyxlim.github.io@a637fef74d91adbd3d8b6c3b27137e73412ac2d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -439,9 +439,9 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+            },},{id: "projects-multistable-energy-landscapes-for-adaptive-microscopic-machines",
+          title: 'Multistable energy landscapes for adaptive microscopic machines',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
